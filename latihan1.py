81e53054c0c0b0afafa495e30923eeb08e2c0802
joysary <Situmeang330@gmail.com>
2025-10-28 16:02:01 +0700

    kondisi if

---
+++ latihan1.py
@@ -0,0 +1,6 @@
+# Latihan 1 - Perulangan bertingkat
+
+for i in range(10):          # baris (0 sampai 9)
+    for j in range(10):      # kolom (0 sampai 9)
+        print(i + j, end=" ")  # jumlahkan i + j dan tampilkan di baris yang sama
+    print()  # pindah ke baris berikutnya
